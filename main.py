9c860c2ff26646345954e473aefb88bd8319baea
kpikpo <capmars@gmail.com>
2024-03-18 11:00:31 +0100

    added accent decoding in utf-8

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ if __name__ == '__main__':
     pron = pandas.read_csv("French-Dictionary-master/dictionary/pron.csv")
     verb = pandas.read_csv("French-Dictionary-master/dictionary/verb.csv")
     listOfEverything=[adj,adv,conj,det,noun,prep,pron,verb,dictionary]
-    text=open("texttocorrect.txt")
+
+
     notFound=[]
-    for line in text:
-        for word in line.split( ):
-            if word.__contains__("'"):
-                pass
-            else:
-                notFound.append(word)
-                for i in listOfEverything:
-                    if word.lower() in i["form"].values:
-                        print(f"found {word.lower()}")
-                        notFound.pop()
-                        break
+    with open("texttocorrect.txt", "r", encoding="utf-8") as text:
+        for line in text:
+            for word in line.split( ):
+                if word.__contains__("'"):
+                    pass
+                else:
+                    notFound.append(word)
+                    for i in listOfEverything:
+                        if word.lower() in i["form"].values:
+                            print(f"found {word.lower()}")
+                            notFound.pop()
+                            break
     text.close()
     print(notFound)
